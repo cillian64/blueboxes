9dba52ba1d598825011632582aaa211d763f7d6c
David Turner <git@dwt27.co.uk>
2020-05-23 21:32:53 +0100

    Update copyright headers
    ChibiOS RT is GPL3, so blueboxes dsp's firmware also is.  Add an appropriate
    header to the source files.  (Leaving the ChibiOS boilerplate with its old
    apache headers from chibios HAL).
    
    Add proper attribution/blame to Adam Greig for the OLED driver cribbed from
    Supervide.

--- dsp/firmware/analog.c
+++ dsp/firmware/analog.c
@@ -1,6 +1,20 @@
 /*
- * BlueBoxes DSP ADC driver. (c) 2016 David Turner blueboxes@dwt27.co.uk
- * Based on M2FC analogue driver, (c) 2015 Eivind Roson Eide, CUSF
+    bluxboxes dsp - Copyright (c) 2016-2020 David Turner
+    Uses ChibiOS RT/HAL - Copyright (C) 2006..2016 Giovanni Di Sirio
+    Based on M2FC analogue driver, (c) 2015 Eivind Roson Eide, CUSF
+
+    blueboxes dsp is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    blueboxes dsp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <ch.h>
--- dsp/firmware/oled.h
+++ dsp/firmware/oled.h
@@ -1,8 +1,21 @@
 /*
- * oled.h
- * OLED Driver
- * Supervide
- */
+    bluxboxes dsp - Copyright (c) 2016-2020 David Turner
+    Uses ChibiOS RT/HAL - Copyright (C) 2006..2016 Giovanni Di Sirio
+    Based on OLED driver from Supervide - (C) 2015 Adam Greig
+
+    blueboxes dsp is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    blueboxes dsp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef OLED_H
 #define OLED_H
